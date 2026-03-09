91da257c7ca23e8e758cc171246eb7ff0ec169aa
Tomek Dobrowolski <tomekjersey@hotmail.com>
2026-03-09 21:36:31 +0000

    refactor: prepared stubs for search functions, BFS and DFS

---
+++ search.py
@@ -0,0 +1,8 @@
+import graph
+
+# TODO: Confirm return type / function of procedures
+def bfs(g: graph.GRAPH_LIST | graph.GRAPH_MATRIX, start: str):
+    pass
+
+def dfs(g: graph.GRAPH_LIST | graph.GRAPH_MATRIX, start: str):
+    pass
