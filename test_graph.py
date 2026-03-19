4649022a4b2b830446a081ac537dae89464167c9
Tomek Dobrowolski <168644385+tomekjersey0@users.noreply.github.com>
2026-03-19 10:46:12 +0000

    feat: graph building based on v and e strings + testing minor

--- test_graph.py
+++ test_graph.py
@@ -1,5 +1,45 @@
 import search
 import graph
+from typing import Any
+
+class Test:
+    def __init__(self, test, expected_output):
+        self.test = test
+        self.exp = expected_output
+
+    def run(self) -> tuple[Any, bool]:
+        out = self.test()
+        return (out == self.exp, out)
+
+def build_graph(vertices: str, edges: str) -> graph:
+    vs = vertices
+    vs_d = set(list(vs))
+    es = list(edges)
+    
+    g = graph.GRAPH_LIST()
+    for v in vs:
+        g.add_vertex(v)
+
+    if len(es) % 2 == 1:
+        es.pop()
+    
+    for i in range(0,len(es),2):
+        e = es[i]
+        if e in vs_d and es[i+1] in vs_d:
+            g.add_edge(e, es[i+1])
+    
+    return g
+
+def t1():
+    vs = "abcdefgh"
+    es = "accafahdahdgfaef"
+    g = build_graph(vs, es)
+    
+    return search.bfs(g,"a")
+
+print(t1())
+
+        
 
 g = graph.GRAPH_LIST()
 g.add_vertex("a")
