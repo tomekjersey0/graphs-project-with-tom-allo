8eae94cb462b3363c76e2dfdd35ac62d012ecf6c
Tomek Dobrowolski <168644385+tomekjersey0@users.noreply.github.com>
2026-03-19 10:20:42 +0000

    refactor: naming changes

--- search.py
+++ search.py
@@ -1,27 +1,26 @@
 import graph
 import queue
-
-
+from typing import Any
 
 # return list of nodes in visitation order
 
 def bfs(g: graph.GRAPH_LIST, start: str) -> list[str]:
-    order: dict[str] = {}
+    visited: dict[str, Any] = {}
 
-    cur_node = start
+    cur = start
     q = queue.Queue(maxsize=0)
-    q.put(cur_node)
+    q.put(cur)
     while not q.empty():
-        cur_node = q.get()
-        order.append(cur_node)
-        for n in g.graph[cur_node]:
-            q.put(n)
-        
-        
+        cur = q.get()
+        if cur not in visited:
+            visited[cur] = None
+            for n in g.graph[cur]:
+                q.put(n)
     
-
+    return list(visited.keys())
+    
 
 
 # return list of nodes in visitation order
 def dfs(g: graph.GRAPH_LIST, start: str) -> list[str]:
-    pass
+    return []
