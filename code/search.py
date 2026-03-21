5ace782c724ab4beac664c199d6dbb26189feed1
Tomek Dobrowolski <tomekjersey@hotmail.com>
2026-03-21 12:10:56 +0000

    perf: improved bfs efficiency

--- code/search.py
+++ code/search.py
@@ -4,27 +4,26 @@ from typing import Any
 
 # return list of vertices in visitation order
 
-def bfs(g: graph.GRAPH_LIST, start: str) -> list[str]:
+def bfs(g: graph.GraphList, start: str) -> list[str]:
     if not g.find_vertex(start):
         return []
 
-    visited: dict[str, Any] = {}
+    visited: dict[str, Any] = {start: None}
     q = deque([start])
 
     while q:
         cur = q.popleft()
-        if cur not in visited:
-            visited[cur] = None
-            for n in g.graph.get(cur, {}):
-                if n not in visited:
-                    q.append(n)
+        for n in g.graph.get(cur, {}):
+            if n not in visited:
+                visited[n] = None
+                q.append(n)
     
     return list(visited)
     
 
 
 # return list of vertices in visitation order
-def dfs(g: graph.GRAPH_LIST, start: str) -> list[str]:
+def dfs(g: graph.GraphList, start: str) -> list[str]:
     if not g.find_vertex(start):
         return []
     
